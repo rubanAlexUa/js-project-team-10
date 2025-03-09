fa3e0c0d8ad28e360d5d91bfecf85304a11ea3a7
Alex <113380721+rubanAlexUa@users.noreply.github.com>
2025-03-09 19:19:05 +0200

    adding swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,3 +1,5 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import axios from 'axios';
+import Swiper from 'swiper';
+import 'swiper/css';
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import Swiper from 'swiper';
+import 'swiper/css';
