947579199102dcb10553c5ea7ec51a3351871c3f
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-03-11 15:54:52 +0200

    fix header

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,22 @@
+const openMenuBtn = document.querySelector('.menu-open-btn');
+const closeMenuBtn = document.querySelector('.mob-menu-close');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuLinks = document.querySelector('.mob-menu-link');
 
+function toggleMenu() {
+  mobileMenu.classList.toggle('hidden');
+
+  if (mobileMenu.classList.contains('hidden')) {
+    document.body.style.overflow = 'auto';
+  } else {
+    document.body.style.overflow = 'hidden';
+  }
+}
+
+openMenuBtn.addEventListener('click', toggleMenu);
+
+closeMenuBtn.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
