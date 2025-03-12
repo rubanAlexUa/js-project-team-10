96b781c0540bd6936271415f4394f8b709861b54
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-03-12 15:10:15 +0200

    about-me

---
+++ src/js/about-me.js
@@ -0,0 +1,41 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.accordion-container').forEach((accordionElement, index) => {
+        try {
+            const acc = new Accordion(accordionElement, {
+                duration: 300,
+                showFirst: false
+            });
+            
+            if (index === 0) {
+                acc.open(0);
+            }
+        } catch (error) {
+            console.error("Accordion initialization error:", error);
+        }
+    });
+});
+
+   document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.mySwiper').forEach((swiperElement, index) => {
+        new Swiper(swiperElement, {
+            loop: true,
+            
+            breakpoints: {
+                320: { slidesPerView: 2 },
+                375: { slidesPerView: 2 },
+                768: { slidesPerView: 3 },
+                1440: { slidesPerView: 6 }
+            },
+            navigation: {
+                nextEl: `.swiper-button-next`, 
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: true,
+            },
+        });
+    });
+});
